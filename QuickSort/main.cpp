a9da8e5cf24993e30f4e28b5088cae617153e0f3
diogoalfa <Diogo@MacBook-Pro-de-Diego.local>
2013-11-08 18:06:18 -0300

    Initial Commit

---
+++ QuickSort/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  QuickSort
+//
+//  Created by Diego Cristobal Navia Fuentes on 08-11-13.
+//  Copyright (c) 2013 Diego Cristobal Navia Fuentes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
